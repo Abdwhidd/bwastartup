4031c0e25abb55d3c443f25062c5cddd1d85952b
Wahid <abdulwahidkaharr@gmail.com>
2023-10-24 13:54:14 +0700

    user

---
+++ handler/user.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct RegisterUserInput
+	// struct diatas kita parsing menjadi parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	response := helper.ApiResponse("Account has be registered", http.StatusOK, "success", user)
+
+	c.JSON(http.StatusOK, response)
+}
---
+++ user/entity.go
@@ -0,0 +1,16 @@
+package user
+
+import "time"
+
+type User struct {
+	Id             int       `json:"id"`
+	Name           string    `json:"name"`
+	Occupation     string    `json:"occupation"`
+	Email          string    `json:"email"`
+	PasswordHash   string    `json:"password_hash"`
+	AvatarFileName string    `json:"avatar_file_name"`
+	Role           string    `json:"role"`
+	Token          string    `json:"token"`
+	CreatedAt      time.Time `json:"created_at" gorm:"default:CURRENT_TIMESTAMP()"`
+	UpdateAt       time.Time `json:"update_at" gorm:"default:CURRENT_TIMESTAMP()"`
+}
