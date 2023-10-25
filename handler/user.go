222b7a26c3fbb101274d7e7fab9e34042f57c1d6
Wahid <abdulwahidkaharr@gmail.com>
2023-10-25 13:20:23 +0700

    update input email

--- handler/user.go
+++ handler/user.go
@@ -36,7 +36,7 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	newUser, err := h.userService.RegisterUser(input)
 
-	if true {
+	if err != nil {
 		response := helper.ApiResponse("Registered account failed", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
@@ -44,6 +44,85 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	formatter := user.FormatterUser(newUser, "tokeennnnnnnnnnn")
 	response := helper.ApiResponse("Account has be registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	// user memasukkan email & password
+	// input ditangkap handler
+	// mapping dari input user ke input struct
+	// input struct passing ke service
+	// diservice mencari dengan bantuan repository user & email
+	// cocokkan password
+
+	var input user.LoginUserInput
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+
+		var errors = helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.ApiResponse("Login Failed", http.StatusUnprocessableEntity, "errors", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.ApiResponse("Login Failed", http.StatusUnprocessableEntity, "errors", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatterUser(newUser, "tokeennnnnnnnnnn")
+	response := helper.ApiResponse("Success Login", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
+	// user memasukkan email
+	// input ditangkap handler
+	// mapping dari input user ke input struct
+	// input struct passing ke service
+	// diservice mencari dengan bantuan repository mencari email
+	// resopitory -> db
+
+	var input user.CheckEmailInput
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		var errors = helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.ApiResponse("Email check Failed", http.StatusUnprocessableEntity, "errors", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	isEmailAvailable, err := h.userService.IsEmailAvailable(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": "Server error"}
+		response := helper.ApiResponse("Email check Failed", http.StatusUnprocessableEntity, "errors", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	data := gin.H{
+		"is_available": isEmailAvailable,
+	}
+
+	metaMessage := "Email has been registered"
+
+	if isEmailAvailable {
+		metaMessage = "Email is available"
+	}
+
+	response := helper.ApiResponse(metaMessage, http.StatusOK, "success", data)
 
 	c.JSON(http.StatusOK, response)
 }
